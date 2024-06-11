dcabcb876319cf920a41d5c54d3581a641a3abc4
vivek <vivekkashyap043@gmail.com>
2024-06-11 19:55:11 +0530

    kk2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 export default async ({ req, res, log, error }) => {
+
+  console.log("request object is: ", req)
   
   console.log("insed the function")
+
   return res.json({ data: "vivek" });
 };
